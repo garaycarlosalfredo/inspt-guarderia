333e8c7eb0c0e431d58702ee8b1deb4d51f8bb06
Carlos Garay <garaycarlosalfredo@gmail.com>
2021-09-03 11:14:47 -0300

    Java SpringBoot and Angular init

---
+++ server/src/main/java/com/inspt/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.inspt.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
