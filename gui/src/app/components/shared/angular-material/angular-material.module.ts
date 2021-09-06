af31179357a3dc1aedbd03ab93a42f8e69c6c87e
Carlos Garay <garaycarlosalfredo@gmail.com>
2021-09-06 10:23:19 -0300

    [DEV][FRONT][Estructura del proyecto angular]

---
+++ gui/src/app/components/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Angular Material
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatTableModule
+  ],
+  exports:[
+    MatSliderModule,
+    MatToolbarModule,
+    MatTableModule
+  ]
+})
+export class AngularMaterialModule { }
