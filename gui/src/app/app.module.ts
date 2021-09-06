c3b546ecf4e2e2977c908fce20d0258ffac23b33
Carlos Garay <garaycarlosalfredo@gmail.com>
2021-09-06 18:07:13 -0300

    [DEV][FRONT][Creado componente login]

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ListEmpleadoComponent } from './components/list-empleado/list-empleado.
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MensajeConfirmacionComponent } from './components/shared/mensaje-confirmacion/mensaje-confirmacion.component';
 import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
+import { LoginComponent } from './components/auth/login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AngularMaterialModule } from './components/shared/angular-material/angu
     AddEditEmpleadoComponent,
     ListEmpleadoComponent,
     NavbarComponent,
-    MensajeConfirmacionComponent
+    MensajeConfirmacionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
